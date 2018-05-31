88f5b21cd664de5b70e2c2d375fa04aa14784ee2
henry.wu <henry.wu@tc-gaming.com>
2018-05-31 15:12:42 +0800

    TCG-18306 修改11x5定单双及猜中位奖金总览显示

--- src/i18n/lang/zh_cn/index.js
+++ src/i18n/lang/zh_cn/index.js
@@ -937,6 +937,19 @@ const LANG = {
     P_FOURTEEN_NINETEEN: '14＆19',
     P_FIFTEEN_SIXTEEN_SEVENTEEN_EIGHTEEN: '15＆16＆17＆18',
 
+    // 11x5 定單雙
+    P_ZERO_S_FIVE_D: '0单5双',
+    P_FIVE_S_ZERO_D: '5单0双',
+    P_ONE_S_FOUR_D: '1单4双',
+    P_FOUR_S_ONE_D: '4单1双',
+    P_TWO_S_THREE_D: '2单3双',
+    P_THREE_S_TWO_D: '3单2双',
+    // 11x5 猜中位
+    P_THREE_NINE: '3＆9',
+    P_FOUR_EIGHT: '4＆8',
+    P_FIVE_SEVEN: '5＆7',
+    P_SIX: '6',
+
     P_NOT_PAIR: '非对子',
     P_PAIR: '对子',
     P_ZU3: '组三',
