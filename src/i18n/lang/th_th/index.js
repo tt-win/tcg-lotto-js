69566f970972181c89e1eccc969014f2357424d8
Taylor Tang <taylor.t@tc-gaming.com>
2018-11-09 20:46:16 +0800

    TCG-22549 thai i18n

---
+++ src/i18n/lang/th_th/index.js
@@ -0,0 +1,14 @@
+import PlayMenu from './playmenu';
+
+const LANG = {
+  ...PlayMenu,
+  shoppingBasket: 'รายการแทง',
+  confirmBetting: 'ส่งโพย',
+  clearBallText: 'ล้างข้อมูล',
+  betNow: 'แทงพนัน',
+  confirmBetInfos: 'ดูโพยอีกครั้งก่อนยืนยันการแทงพนัน',
+  balance: 'เครดิตเครดิต',
+  totalAmount: 'จำนวนเงินทั้งหมด',
+};
+
+export default LANG;
