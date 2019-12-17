0da3c3d9d69576ad67914f1d6e4a5853d2514d92
Yuren Kuo <yuren.k@tc-gaming.com>
2019-12-17 17:21:34 +0800

    TCG-36250 福彩3D走势图组态

--- src/config/trend-chart/index.js
+++ src/config/trend-chart/index.js
@@ -2,10 +2,12 @@ import trendChartConfigSSC from './trend_chart_config_ssc';
 import trendChartConfigST from './trend_chart_config_st';
 import trendChartConfigPK10 from './trend_chart_config_pk10';
 import trendChartConfigP3P5 from './trend_chart_config_p3p5';
+import trendChartConfig3D from './trend_chart_config_3d';
 
 export const trendChartConfig = {
   ...trendChartConfigSSC,
   ...trendChartConfigST,
   ...trendChartConfigPK10,
   ...trendChartConfigP3P5,
+  ...trendChartConfig3D,
 };
