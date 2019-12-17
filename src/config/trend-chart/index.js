80f85753bf43ebc1738a8a9d1d5eb8291343818c
joanne.ch <Drummer_1002>
2019-12-17 18:08:05 +0800

    TCG-36297 [Lotto|UI] PCB28走势图组态 config

--- src/config/trend-chart/index.js
+++ src/config/trend-chart/index.js
@@ -6,6 +6,7 @@ import trendChartConfig11X5 from './trend_chart_config_11x5';
 import trendChartConfig4d from './trend_chart_config_4d';
 import trendChartConfig3D from './trend_chart_config_3d';
 import trendChartConfigLHC from './trend_chart_config_lhc';
+import trendChartConfigPCB28 from './trend_chart_config_pcb28';
 
 export const trendChartConfig = {
   ...trendChartConfigSSC,
@@ -16,4 +17,5 @@ export const trendChartConfig = {
   ...trendChartConfig4d,
   ...trendChartConfig3D,
   ...trendChartConfigLHC,
+  ...trendChartConfigPCB28,
 };
---
+++ src/config/trend-chart/trend_chart_config_pcb28.js
@@ -0,0 +1,23 @@
+/*
+  numero            期號
+  digit             顯示位數
+  special           特码
+  specialBS         特码大小
+  specialOE         特码单双
+  extremum          极值
+  colorBall         色波
+  baozi             豹子
+*/
+
+const PCB28 = {
+  list: {
+    Com: {
+      column: ['numero', 'digit', 'special', 'specialBS', 'specialOE', 'extremum', 'colorBall', 'baozi'],
+      options: {
+        digit: ['firstBall', 'secondBall', 'thirdBall'],
+      },
+    },
+  },
+};
+
+export default PCB28;
