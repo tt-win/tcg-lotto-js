a456eaedb2adfc3c74c15e2b96c27539b4d4ca53
Yuren Kuo <yuren.k@tc-gaming.com>
2019-12-17 17:04:39 +0800

    TCG-36266 体彩P3P5走势图组态

--- src/config/trend-chart/index.js
+++ src/config/trend-chart/index.js
@@ -1,9 +1,11 @@
 import trendChartConfigSSC from './trend_chart_config_ssc';
 import trendChartConfigST from './trend_chart_config_st';
 import trendChartConfigPK10 from './trend_chart_config_pk10';
+import trendChartConfigP3P5 from './trend_chart_config_p3p5';
 
 export const trendChartConfig = {
   ...trendChartConfigSSC,
   ...trendChartConfigST,
   ...trendChartConfigPK10,
+  ...trendChartConfigP3P5,
 };
