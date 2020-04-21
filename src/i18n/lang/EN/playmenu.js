aaaa62ac221bb21f955209800bac5423c3da565f
george.k <george.k@tc-gaming.com>
2020-04-21 18:47:43 +0800

    TCG-39955 东南亚彩英/简中/繁中玩法翻译

--- src/i18n/lang/EN/playmenu.js
+++ src/i18n/lang/EN/playmenu.js
@@ -7,45 +7,45 @@ const PlayMenu = {
     2063: 'Boxed Bet', // 组选
     2065: 'Bonus digit', // 直选加开
     2068: 'Random Back 3 Digits', // 后三不定位
-    2055: 'Pick-3', // 三数
-    2057: 'Front 3-Digit', // 上正三位数
-    2059: 'Middle 3-Digit', // 上反三位数
-    2060: 'Pick-2', // 二数
-    2062: 'Front 2-Digit', // 上正二位数
-    2064: 'Middle 2-Digit', // 上反二位数
-    2066: 'Back 2-Digit', // 下正二位数
-    2067: 'Chasing number', // 跑数
-    2069: 'Random Front 3 Digits', // 上跑三位数
+    2055: 'Three Digit', // 三数
+    2057: 'EXACT 3D-1st', // 上正三位数
+    2059: 'ANY 3D-1st', // 上反三位数
+    2060: 'Two Digit', // 二数
+    2062: 'EXACT 2D-1st', // 上正二位数
+    2064: 'ANY 2D-1st', // 上反二位数
+    2066: 'EXACT 2D-Special ', // 下正二位数
+    2067: 'One Digit', // 跑数
+    2069: 'ANY 1D-1st', // 上跑三位数
 
     // vnc 越南彩
-    2090: 'Pick-4', // 四数
-    2095: 'Pick-3', // 三数
-    2100: 'Pick-2', // 二数
-    2105: 'Random Number', // 跑数
+    2090: 'Four Digit', // 四数
+    2095: 'Three Digit', // 三数
+    2100: 'Two Digit', // 二数
+    2105: 'One Digit', // 跑数
 
-    2092: 'Front 4-Digit', // 上正四位数 Last_4_Straight_1st_VNC
-    2094: 'Back 4-Digit', // 上反四位数 Last_4_Combine_1st_VNC
-    2097: 'Front 3-Digit', // 上正三位数 Last_3_Straight_1st_VNC
-    2099: 'Middle 3-Digit', // 上反三位数 Last_3_Combine_1st_VNC
-    2102: 'Front 2-Digit', // 上正二位数 Last_2_Straight_1st_VNC
-    2104: 'Back 2-Digit', // 下正二位数 Last_2_Straight_2nd_VNC
-    2107: 'Random Front 2 Digits', // 上跑两位数 Last_2_Any_1st_VNC
-    2109: 'Random Back 2 Digits', // 下跑两位数 Last_2_Any_2nd_VNC
+    2092: 'EXACT 4D-1st', // 上正四位数 Last_4_Straight_1st_VNC
+    2094: 'ANY 4D-1st', // 上反四位数 Last_4_Combine_1st_VNC
+    2097: 'EXACT 3D-1st', // 上正三位数 Last_3_Straight_1st_VNC
+    2099: 'ANY 3D-1st', // 上反三位数 Last_3_Combine_1st_VNC
+    2102: 'EXACT 2D-1st', // 上正二位数 Last_2_Straight_1st_VNC
+    2104: 'ANY 3D-2nd', // 下正二位数 Last_2_Straight_2nd_VNC
+    2107: 'ANY 1D-1st', // 上跑两位数 Last_2_Any_1st_VNC
+    2109: 'ANY 1D-2nd', // 下跑两位数 Last_2_Any_2nd_VNC
 
     // LAO 老挝彩
-    2127: 'Pick-4', // 四数
-    2132: 'Pick-3', // 三数
-    2137: 'Pick-2', // 二数
-    2142: 'Random Numbers', // 跑数
+    2127: 'Four Digit', // 四数
+    2132: 'Three Digit', // 三数
+    2137: 'Two Digit', // 二数
+    2142: 'One Digit', // 跑数
 
-    2129: 'Front 4 digit', // 上正四位数 Last_4_Straight_LAO
-    2131: 'Back 4 digit', // 上反四位数 Last_4_Combine_LAO
-    2134: 'Front 3-Digit', // 上正三位数 Last_3_Straight_LAO
-    2136: 'Back 3-Digit', // 上反三位数 Last_3_Combine_LAO
-    2139: 'Front 2-Digit', // 上正二位数 Last_2_Straight_LAO
-    2141: 'Back 2-Digit', // 下正二位数 First_2_Straight_LAO
-    2144: 'Random Front 2 Digits', // 上跑两位数 Last_2_Any_LAO
-    2146: 'Random Back 2 Digits', // 下跑两位数 First_2_Any_LAO
+    2129: 'EXACT 4D', // 上正四位数 Last_4_Straight_LAO
+    2131: 'ANY 4D', // 上反四位数 Last_4_Combine_LAO
+    2134: 'EXACT 3D', // 上正三位数 Last_3_Straight_LAO
+    2136: 'ANY 3D', // 上反三位数 Last_3_Combine_LAO
+    2139: 'EXACT 2D-last', // 上正二位数 Last_2_Straight_LAO
+    2141: 'EXACT 2D-first', // 下正二位数 First_2_Straight_LAO
+    2144: 'ANY 1D-last', // 上跑两位数 Last_2_Any_LAO
+    2146: 'ANY 1D-first', // 下跑两位数 First_2_Any_LAO
   },
 };
 
