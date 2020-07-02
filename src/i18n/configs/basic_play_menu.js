1f96ededec5d1ccbb07a7cbce5f864202bf78b7b
Yuren Kuo <yuren.k@tc-gaming.com>
2020-07-02 17:24:56 +0800

    TCG-40720 鱼虾蟹玩法

--- src/i18n/configs/basic_play_menu.js
+++ src/i18n/configs/basic_play_menu.js
@@ -840,15 +840,18 @@ export const PlayMenu = {
   Last_3_THAI: 2055,
   '2_Star_THAI': 2060,
   Any_Place_THAI: 2067,
+  Hoo_Hey_How_THAI: 2299,
   Last_4_VNC: 2090, // 越南彩四數
   Last_3_VNC: 2095,
   '2_Star_VNC': 2100,
   Any_Place_VNC: 2105,
+  Hoo_Hey_How_VNC: 2302,
   // 老撾彩
   '4_Num_LAO': 2127,
   '3_Num_LAO': 2132,
   '2_Num_LAO': 2137,
   Any_LAO: 2142,
+  Hoo_Hey_How_LAO: 2305,
 
   LAST_3_STRAIGHT_THAI: 2057, // 上正三位数
   LAST_3_COMBINE_THAI: 2059, // 上反三位数
