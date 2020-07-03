2e29fe2acfbc7f759216af37d4e227eb387112a5
joanne.ch <joanne.ch@tc-gaming.com>
2020-07-03 16:30:04 +0800

    TCG-40720 [Lotto|TCGSEA] 泰彩、自主3D 新增 鱼虾蟹玩法

--- src/i18n/configs/basic_play_menu.js
+++ src/i18n/configs/basic_play_menu.js
@@ -840,7 +840,7 @@ export const PlayMenu = {
   Last_3_THAI: 2055,
   '2_Star_THAI': 2060,
   Any_Place_THAI: 2067,
-  Hoo_Hey_How_THAI: 2297, // 魚蝦蟹
+  Hoo_Hey_How_Main_THAI: 2297, // 魚蝦蟹
   Hoo_Hey_How_THAI: 2299, // 魚蝦蟹
 
   LAST_3_STRAIGHT_THAI: 2057, // 上正三位数
@@ -864,7 +864,7 @@ export const PlayMenu = {
   Last_3_VNC: 2095,
   '2_Star_VNC': 2100,
   Any_Place_VNC: 2105,
-  Hoo_Hey_How_VNC: 2300, // 魚蝦蟹
+  Hoo_Hey_How_Main_VNC: 2300, // 魚蝦蟹
   Hoo_Hey_How_VNC: 2302, // 魚蝦蟹
 
   LAST_4_STRAIGHT_1ST_VNC: 2092, // 上正四位数
@@ -891,7 +891,7 @@ export const PlayMenu = {
   '3_Num_LAO': 2132,
   '2_Num_LAO': 2137,
   Any_LAO: 2142,
-  Hoo_Hey_How_LAO: 2303, // 魚蝦蟹
+  Hoo_Hey_How_Main_LAO: 2303, // 魚蝦蟹
   Hoo_Hey_How_LAO: 2305, // 魚蝦蟹
 
   LAST_4_STRAIGHT_LAO: 2129, // 上正四位数
