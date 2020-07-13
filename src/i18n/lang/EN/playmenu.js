e8f5406faff4d3c36b86aa1a6eccba74c5a4c8d1
Taylor Tang <taylor.t@tc-gaming.com>
2020-07-13 17:11:27 +0800

    TCG-42449 update translate

--- src/i18n/lang/EN/playmenu.js
+++ src/i18n/lang/EN/playmenu.js
@@ -7,14 +7,14 @@ const PlayMenu = {
     2063: 'Boxed Bet', // 组选
     2065: 'Bonus digit', // 直选加开
     2068: 'Random Back 3 Digits', // 后三不定位
-    2055: 'Three Digit', // 三数
+    2055: '3D', // 三数
     2057: 'EXACT 3D-1st', // 上正三位数
     2059: 'ANY 3D-1st', // 上反三位数
-    2060: 'Two Digit', // 二数
+    2060: '2D', // 二数
     2062: 'EXACT 2D-1st', // 上正二位数
     2064: 'ANY 2D-1st', // 上反二位数
     2066: 'EXACT 2D-Special ', // 下正二位数
-    2067: 'One Digit', // 跑数
+    2067: '1D', // 跑数
     2069: 'ANY 1D-1st', // 上跑三位数
 
     2480: 'Norm EXACT 2D-Special ',
@@ -30,10 +30,10 @@ const PlayMenu = {
     2299: 'FishPrawnCrab', // 魚蝦蟹
 
     // vnc 越南彩
-    2090: 'Four Digit', // 四数
-    2095: 'Three Digit', // 三数
-    2100: 'Two Digit', // 二数
-    2105: 'One Digit', // 跑数
+    2090: '4D', // 四数
+    2095: '3D', // 三数
+    2100: '2D', // 二数
+    2105: '1D', // 跑数
 
     2092: 'EXACT 4D-1st', // 上正四位数 Last_4_Straight_1st_VNC
     2094: 'ANY 4D-1st', // 上反四位数 Last_4_Combine_1st_VNC
@@ -59,10 +59,10 @@ const PlayMenu = {
     2302: 'FishPrawnCrab', // 魚蝦蟹
 
     // LAO 老挝彩
-    2127: 'Four Digit', // 四数
-    2132: 'Three Digit', // 三数
-    2137: 'Two Digit', // 二数
-    2142: 'One Digit', // 跑数
+    2127: '4D', // 四数
+    2132: '3D', // 三数
+    2137: '2D', // 二数
+    2142: '1D', // 跑数
 
     2129: 'EXACT 4D', // 上正四位数 Last_4_Straight_LAO
     2131: 'ANY 4D', // 上反四位数 Last_4_Combine_LAO
