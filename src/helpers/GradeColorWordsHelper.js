29a2bc7b64bbbd2b509c4e36ddf03a0d6c2512ac
zizi <zizi.h.tcg@gmail.com>
2020-10-15 17:37:35 +0800

    TCG-44542 新增開獎位數玩法

--- src/helpers/GradeColorWordsHelper.js
+++ src/helpers/GradeColorWordsHelper.js
@@ -34,7 +34,29 @@ export const getGradeNumberOfColorWords = (playId) => {
       return { ...baseProps, numberOfColorWords: 4 };
       // 上正四位数 test
     case PlayMenu.LAST_4_STRAIGHT_1ST_VNC:
+    case PlayMenu.LAST_4_COMBINE_1ST_VNC:
+    case PlayMenu.Last_4_Straight_1st_VNC_Multi:
+    case PlayMenu.Last_4_Straight_1st_VNC_Single:
       return { grade: [0], numberOfColorWords: 4 }; // 特等獎 後四位
+    case PlayMenu.LAST_3_STRAIGHT_1ST_VNC:
+    case PlayMenu.LAST_3_COMBINE_1ST_VNC:
+    case PlayMenu.Last_3_Straight_1st_VNC_Multi:
+    case PlayMenu.Last_3_Combine_1st_VNC_Multi:
+    case PlayMenu.Last_3_Straight_1st_VNC_Single:
+    case PlayMenu.Fan_Tan_Fan_VNC:
+      return { grade: [0], numberOfColorWords: 3 }; // 特等獎 後三位
+    case PlayMenu.LAST_2_STRAIGHT_1ST_VNC:
+    case PlayMenu.Last_2_Straight_1st_VNC_Multi:
+    case PlayMenu.Last_2_Straight_1st_VNC_Single:
+    case PlayMenu.LAST_2_ANY_1ST_VNC:
+      return { grade: [0], numberOfColorWords: 2 }; // 特等獎 後二位
+    case PlayMenu.Hoo_Hey_How_Play_VNC:
+      return { grade: [0], numberOfColorWords: 1 }; // 特等獎 後一位
+    case PlayMenu.LAST_2_STRAIGHT_2ND_VNC:
+    case PlayMenu.Last_2_Straight_2nd_VNC_Multi:
+    case PlayMenu.Last_2_Straight_2nd_VNC_Single:
+    case PlayMenu.LAST_2_ANY_2ND_VNC:
+      return { grade: [1], numberOfColorWords: 2 }; // 一等獎 後二位
     default:
       return baseProps;
   }
