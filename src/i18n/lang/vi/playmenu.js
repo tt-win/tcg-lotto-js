4fd998da3865314af26c9a6cb13729584ccdc056
Taylor Tang <taylor.t@tc-gaming.com>
2021-04-06 18:00:26 +0800

    TCG-50335 update thai lao playmenu i18n

--- src/i18n/lang/vi/playmenu.js
+++ src/i18n/lang/vi/playmenu.js
@@ -8,22 +8,22 @@ const PlayMenu = {
     2065: 'chọn ngay thêm mở',
     2068: 'sau 3 không định vị',
     2055: '3 số', // 三数
-    2057: 'trên ngay 3 chữ số',
-    2059: 'trên ngược 3 chữ số',
+    2057: '3 Càng Đặc Biệt', // 上正三位数
+    2059: 'Xiên 3 Đặc Biệt', // 上反三位数
     2060: '2 số', // 二数
-    2062: 'trên ngay 2 chữ số',
-    2064: 'trên ngược 2 chữ số',
-    2066: 'dưới ngay 2 chữ số',
+    2062: 'Đề Đặc Biệt', // 上正二位数 Last_2_Straight_1st_THAI
+    2064: 'Xiên 2 Giải Nhất',
+    2066: 'Đề Giải Nhất', // 下正二位数 Last_2_Straight_2nd_THAI
     2067: 'chạy số', // 跑数
-    2069: 'trên chạy 3 chữ số',
-    2480: 'Chọn Số Hai chữ số chính xác - Giải Số Đặc Biệt',
-    2481: 'Nhập Số Hai chữ số chính xác - Giải Số Đặc Biệt',
-    2482: 'Chọn Số Hai chữ số bất kỳ',
-    2483: 'Chọn Số Hai chữ số chính xác',
-    2484: 'Nhập Số Hai chữ số chính xác',
-    2485: 'Chọn Số Ba chữ số đầu chính xác',
-    2486: 'Nhập Số Ba chữ số đầu chính xác',
-    2487: 'Chọn Số Ba chữ số bất kỳ',
+    2069: 'Xiên Số Giải Nhất',
+    2480: 'Đề Giải Nhất- Chọn Số', // 下正二位数（复式）Last_2_Straight_2nd_THAI_Multi
+    2481: 'Đề Giải Nhất - Nhập Số', // 下正二位数（单式）Last_2_Straight_2nd_THAI_Single
+    2482: 'Xiên 2 Giải Nhất - Chọn Số',
+    2483: 'Đề Đặc Biệt - Chọn Số', // 上正二位数(复式) Last_2_Straight_1st_THAI_Multi
+    2484: 'Đề Đặc Biệt - Nhập Số', // 上正二位数（单式）Last_2_Straight_1st_THAI_Single
+    2485: '3 Càng Đặc Biệt - Chọn Số', // 上正三位数（复式）
+    2486: '3 Càng Đặc Biệt - Nhập Số', // 上正三位数（单式）
+    2487: 'Xiên 3 Đặc Biệt - Chọn Số', // 上反三位数（复式）Last_3_Combine_1st_TAHI_Multi
 
     2297: 'cược Bầu Cua', // 魚蝦蟹
     2298: 'cược Bầu Cua', // 魚蝦蟹
@@ -234,24 +234,24 @@ const PlayMenu = {
     2138: 'Chính Xác', // 上正
     2140: 'Chính Xác', // 下正
 
-    2129: 'trên ngay 4 chữ số', // 上正四位数 Last_4_Straight_LAO
-    2131: 'trên ngược 4 chữ số', // 上反四位数 Last_4_Combine_LAO
-    2134: 'trên ngay 3 chữ số', // 上正三位数 Last_3_Straight_LAO
-    2136: 'trên ngược 3 chữ số', // 上反三位数 Last_3_Combine_LAO
-    2139: 'trên ngay 2 chữ số', // 上正二位数 Last_2_Straight_LAO
-    2141: 'dưới ngay 2 chữ số', // 下正二位数 First_2_Straight_LAO
-    2144: 'trên chạy 2 chữ số', // 上跑两位数 Last_2_Any_LAO
-    2146: 'dưới chạy 2 chữ số', // 下跑两位数 First_2_Any_LAO
+    2129: '4 Càng Đặc Biệt', // 上正四位数 Last_4_Straight_1st_LAO
+    2131: 'Xiên 4 Đặc Biệt', // 上反四位数 Last_4_Combine_1st_LAO
+    2134: '3 Càng Đặc Biệt', // 上正三位数
+    2136: 'Xiên 3 Đặc Biệt', // 上反三位数 Last_3_Combine_1st_LAO
+    2139: 'Đề Đặc Biệt', // 上正二位数 Last_2_Straight_1st_LAO
+    2141: 'Đề Giải Nhất', // 下正二位数 Last_2_Straight_2nd_LAO
+    2144: 'Xiên Đăc Biệt', // 上跑两位数 Last_2_Any_1st_LAO
+    2146: 'Xiên Giải Nhất', // 下跑两位数 Last_2_Any_2nd_LAO
 
-    2288: 'Chọn Số Hai chữ số chính xác', // Last_2_Straight_LAO_Multi
-    2289: 'Nhập Số Hai chữ số chính xác', // Last_2_Straight_LAO_Single
-    2290: 'Chọn Số Hai chữ số chính xác', // First_2_Straight_LAO_Multi
-    2291: 'Nhập Số Hai chữ số chính xác', // First_2_Straight_LAO_Single
-    2292: 'Chọn Số Ba chữ số bất kỳ', // Last_3_Combine_LAO_Multi
-    2293: 'Chọn Số Ba chữ số đầu chính xác', // Last_3_Straight_LAO_Multi
-    2294: 'Nhập Số Ba chữ số đầu chính xác', // Last_3_Straight_LAO_Single
-    2295: 'Chọn Số Bốn chữ số chính xác', // Last_4_Straight_LAO_Multi
-    2296: 'Nhập Số Bốn chữ số chính xác', // Last_4_Straight_LAO_Single
+    2288: 'Đề Đặc Biệt - Chọn Số', // 上正二位数(复式) Last_2_Straight_1st_LAO_Multi
+    2289: 'Đề Đặc Biệt - Nhập Số', // 上正二位数（单式）Last_2_Straight_1st_LAO_Single
+    2290: 'Đề Giải Nhất- Chọn Số', // 下正二位数（复式）Last_2_Straight_2nd_LAO_Multi
+    2291: 'Đề Giải Nhất - Nhập Số', // 下正二位数（单式）Last_2_Straight_2nd_LAO_Single
+    2292: 'Xiên 3 Đặc Biệt - Chọn Số', // 上反三位数（复式）Last_3_Combine_1st_lao_Multi
+    2293: '3 càng đặc biệt - chọn số', // 上正三位数（复式）last_3_straight_1st_lao_multi
+    2294: '3 càng đặc biệt - nhập số', // 上正三位数（单式）last_3_straight_1st_lao_single
+    2295: '4 Càng Đặc Biệt - Chọn Số', // 上正四位数（复式）Last_4_Straight_1st_LAO_Multi
+    2296: '4 Càng Đặc Biệt - Nhập Số', // 上正四位数（单式）Last_4_Straight_1st_LAO_Single
 
     2303: 'cược Bầu Cua', // 魚蝦蟹
     2304: 'cược Bầu Cua', // 魚蝦蟹
