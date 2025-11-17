f7097f043654b5528d6020b67fc3c0fccc8cf6a0
Lucy Hsu <lucy.h2.tcg@gmail.com>
2025-11-17 14:59:21 +0800

    TCG-118970 SEAPK10玩法說明

--- src/i18n/lang/ty/playmenu.js
+++ src/i18n/lang/ty/playmenu.js
@@ -895,21 +895,21 @@ const PlayMenu = {
 
     // SEAPK10-Level:1
 
-    3100: 'Ranking', // SEA SEAPK10 第1-10名排名
-    3101: '1-10 BigSmall', // SEA SEAPK10 第1-10名大小
-    3102: '1-10 OddEven', // SEA SEAPK10 第1-10名單雙
+    3100: 'Ranggo', // SEA SEAPK10 第1-10名排名
+    3101: '1-10 Malaki/Maliit', // SEA SEAPK10 第1-10名大小
+    3102: '1-10 Bihira/Pares', // SEA SEAPK10 第1-10名單雙
 
     // SEAPK10-Level:2
 
-    3103: 'Ranking', // SEA SEAPK10 第1-10名排名
-    3104: '1-10 BigSmall', // SEA SEAPK10 第1-10名大小
-    3105: '1-10 OddEven', // SEA SEAPK10 第1-10名單雙
+    3103: 'Ranggo', // SEA SEAPK10 第1-10名排名
+    3104: '1-10 Malaki/Maliit', // SEA SEAPK10 第1-10名大小
+    3105: '1-10 Bihira/Pares', // SEA SEAPK10 第1-10名單雙
 
     // SEAPK10-Level:3
 
-    3106: 'Ranking', // SEA SEAPK10 排名
-    3107: 'Big/Small', // SEA SEAPK10 大小
-    3108: 'Odd/Even', // SEA SEAPK10 單雙
+    3106: 'Ranggo', // SEA SEAPK10 排名
+    3107: 'Malaki/Maliit', // SEA SEAPK10 大小
+    3108: 'Bihira/Pares', // SEA SEAPK10 單雙
 
     // mini game mines
 
